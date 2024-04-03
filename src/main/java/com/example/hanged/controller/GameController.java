1c14231c8cebdee9ab04100709209745f004175b
johnramosg <ramos.john@correounivalle.edu.co>
2024-04-03 14:54:19 -0500

    Agregue una alerta para palabras de longitud 0 o menores

--- src/main/java/com/example/hanged/controller/GameController.java
+++ src/main/java/com/example/hanged/controller/GameController.java
@@ -3,6 +3,7 @@ package com.example.hanged.controller;
 import com.example.hanged.model.SecretWord;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -19,16 +20,14 @@ public class GameController {
     @FXML
     public void onHandleButtonPlay(ActionEvent event){
         wordsTxts= new TextField[secretWord.getWord().length()];
-        for (int i=0; i<secretWord.getWord().length(); i++){
+        for (int i=0; i<secretWord.getWord().length(); i++) {
             TextField wordTxt = new TextField();
             wordTxt.setPrefWidth(30);
             gameHBoxLayout.getChildren().add(wordTxt);
             //keyPressed(wordTxt, i);
             //wordsTxts[i] = wordTxt;
             buttonPlay.setVisible(false);
-
-        }
-
+            }
    }
     public void setSecretWord(SecretWord secretWord) {
         this.secretWord = secretWord;
--- src/main/java/com/example/hanged/controller/WelcomeController.java
+++ src/main/java/com/example/hanged/controller/WelcomeController.java
@@ -5,6 +5,7 @@ import com.example.hanged.view.GameStage;
 import com.example.hanged.view.WelcomeStage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
@@ -15,9 +16,25 @@ public class WelcomeController {
     private TextField secretWordTextField;
     @FXML
     public void onHandleButtonPlay(ActionEvent event) throws IOException {
-        SecretWord secretWord = new SecretWord(secretWordTextField.getText().trim());
-        GameStage.getInstance().getGameController().setSecretWord(secretWord);
-        WelcomeStage.deleteInstance();
-        System.out.println(secretWord.getWord());
+        if(secretWordTextField.getText().length()<=0) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+            // Establecer el título de la alerta
+            alert.setTitle("Error");
+
+            // Establecer el encabezado de la alerta
+            alert.setHeaderText("Palabra demasiado corta");
+
+            // Establecer el contenido de la alerta
+            alert.setContentText("Por favor regrese e ingrese una palabra valida");
+
+            // Mostrar la alerta y esperar a que el usuario la cierre
+            alert.showAndWait();
+        }else {
+            SecretWord secretWord = new SecretWord(secretWordTextField.getText().trim());
+            GameStage.getInstance().getGameController().setSecretWord(secretWord);
+            WelcomeStage.deleteInstance();
+            System.out.println(secretWord.getWord());
+        }
     }
 }
