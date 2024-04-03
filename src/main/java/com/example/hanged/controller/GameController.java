d24e98afb3c55b32528da3368b23454addd9fe51
johnramosg <ramos.john@correounivalle.edu.co>
2024-04-03 16:00:04 -0500

    Agregue el boton ingresar nueva palabra

--- src/main/java/com/example/hanged/controller/GameController.java
+++ src/main/java/com/example/hanged/controller/GameController.java
@@ -1,12 +1,17 @@
 package com.example.hanged.controller;
 
 import com.example.hanged.model.SecretWord;
+import com.example.hanged.view.GameStage;
+import com.example.hanged.view.WelcomeStage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+
+import java.io.IOException;
 
 public class GameController {
     @FXML
@@ -18,6 +23,10 @@ public class GameController {
 
     private SecretWord secretWord;
     @FXML
+    private Button buttonInsertLetter;
+    public void ocultarButton(){
+
+    }
     public void onHandleButtonPlay(ActionEvent event){
         wordsTxts= new TextField[secretWord.getWord().length()];
         for (int i=0; i<secretWord.getWord().length(); i++) {
@@ -27,7 +36,20 @@ public class GameController {
             //keyPressed(wordTxt, i);
             //wordsTxts[i] = wordTxt;
             buttonPlay.setVisible(false);
+            wordTxt.setDisable(true);
             }
+   }
+   @FXML
+   public void onHandleButtonInsert(ActionEvent event){
+       System.out.println(secretWord.getWord());
+       }
+   public void onHandleButtonHelp(ActionEvent event){
+       System.out.println("Help");
+   }
+   @FXML
+   public void onHandleButtonBackWelcome(ActionEvent event) throws IOException {
+       WelcomeStage.getInstance();
+       GameStage.deleteInstance();
    }
     public void setSecretWord(SecretWord secretWord) {
         this.secretWord = secretWord;
